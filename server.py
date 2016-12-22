72e3104c9a36fbd23f536add65206fbbcbb6d6bd
Daniel Robinson <d7robin@gmail.com>
2016-12-22 17:53:29 +0200

    server listens on port 8000, streams to connection

---
+++ server.py
@@ -0,0 +1,29 @@
+import socket
+import subprocess
+import time
+import picamera
+
+# Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
+# all interfaces)
+server_socket = socket.socket()
+server_socket.bind(('0.0.0.0', 8000))
+server_socket.listen(0)
+
+# Accept a single connection and make a file-like object out of it
+connection = server_socket.accept()[0].makefile('rb')
+
+try:
+    camera = picamera.PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 24
+    # Start a preview and let the camera warm up for 2 seconds
+    camera.start_preview()
+    time.sleep(2)
+    # Start recording, sending the output to the connection for 60
+    # seconds, then stop
+    camera.start_recording(connection, format='h264')
+    camera.wait_recording(60)
+    camera.stop_recording()
+finally:
+    connection.close()
+    client_socket.close()
