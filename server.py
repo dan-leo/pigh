527d3ccc1f39e1f6438998d119e21a0f4b79e9f3
Daniel Robinson <d7robin@gmail.com>
2016-12-23 19:17:22 +0000

    600 seconds

--- server.py
+++ server.py
@@ -6,11 +6,12 @@ import picamera
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
 # all interfaces)
 server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 8001))
+server_socket.bind(('0.0.0.0', 8000))
 server_socket.listen(0)
 
 # Accept a single connection and make a file-like object out of it
 connection = server_socket.accept()[0].makefile('rb')
+print connection
 
 try:
     camera = picamera.PiCamera()
@@ -22,7 +23,7 @@ try:
     # Start recording, sending the output to the connection for 60
     # seconds, then stop
     camera.start_recording(connection, format='h264')
-    camera.wait_recording(60)
+    camera.wait_recording(600)
     camera.stop_recording()
 except KeyboardInterrupt:
 	pass
